39c4177b15217e756622e9534356b9a76477aa0a
Edgar Lopez <Edgineer@users.noreply.github.com>
2018-03-04 18:53:36 -0800

    Add files via upload
    one minute timer module

---
+++ timer.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module timer(
+input clock,
+output timesup
+);
+
+localparam MINUTE = 60;
+reg timesup_reg = 0;
+reg [5:0] counter = 6'b000000;
+
+always @ (posedge clock) begin
+ 
+ if(counter == MINUTE-1) begin
+  counter <= 6'b0;
+  timesup_reg <= ~timesup_reg;
+ end
+ 
+ else begin
+  counter <= counter + 6'b1;
+  timesup_reg <= timesup_reg;
+ end	
+end
+
+assign timesup = timesup_reg;
+
+endmodule
---
+++ timer_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module timer_tb;
+
+	// Inputs
+	reg clock;
+
+	// Outputs
+	wire timesup;
+
+	// Instantiate the Unit Under Test (UUT)
+	timer uut (
+		.clock(clock),
+		.timesup(timesup)
+	);
+
+	initial begin
+	 // Initialize Inputs
+	 clock = 0;
+	 // Wait 100 ns for global reset to finish
+	 #1000;
+	end
+	
+	always begin
+		#10 clock = ~clock;
+	end
+      
+endmodule
