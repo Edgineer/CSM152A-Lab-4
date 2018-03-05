6fc3a1153a5f0f53aed965e7ff90264084a32e44
Edgar Lopez <Edgineer@users.noreply.github.com>
2018-03-04 20:58:58 -0800

    Add files via upload
    dividers and debouncer

---
+++ debouncer.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module debouncer(
+	//Inputs
+		input btn,
+		input clk,
+	//Outputs
+		output btn_state
+    );
+
+reg state = 0;
+reg [15:0] counter; // 763 Hz sampling rate
+
+always @ (posedge clk) begin
+	if (btn) begin
+		// add to counter & only use state when counter is full
+		counter <= counter + 1'b1;
+		if (counter == 16'b1111111111111111) begin
+			state <= 1;
+			counter <= 0;
+		end
+	end
+	else
+	begin
+		state <= 0;
+		counter <= 0;
+	end
+end
+
+assign btn_state = state;
+
+endmodule
