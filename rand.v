c7e464f86d035873b7dfe7399ebc7e021dab8aea
Edgar Lopez <Edgineer@users.noreply.github.com>
2018-03-04 19:10:19 -0800

    Add files via upload
    random number generator

---
+++ rand.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module rand8bits (
+//Inputs
+    input clock,
+    input reset,
+//Outputs
+    output [7:0] rand 
+);
+ 
+reg [7:0] random, next_rand, done_rand;
+reg [3:0] count, next_count; //count number of shifts 
+
+wire taps = random[7] ^ random[5] ^ random[4] ^ random[3]; //"random" bit
+ 
+always @ (posedge clock or posedge reset) begin
+ if (reset) begin
+  random <= 8'hFF; //An LFSR cannot have an all 0 state, thus reset to FF
+  count <= 0;
+ end
+  
+ else begin
+  random <= next_rand;
+  count <= next_count;
+ end
+end
+ 
+always @ (*) begin
+ next_rand = random;
+ next_count = count;
+   
+ next_rand = {random[6:0], taps}; //left shift insert the xor'd bit on every posedge clock
+ next_count = count + 1;
+ 
+ if (count == 8) begin
+  count = 0;
+  done_rand = random; //assign the random number to output after 8 shifts
+ end
+end
+
+assign rand = done_rand;
+
+endmodule
---
+++ rand_tb.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module test;
+ 
+ // Inputs
+ reg clock;
+ reg reset;
+ 
+ // Outputs
+ wire [7:0] rand;
+ 
+ // Instantiate the Unit Under Test (UUT)
+ rand8bits uut (
+  .clock(clock), 
+  .reset(reset), 
+  .rand(rand)
+ );
+  
+ initial begin
+  clock = 0;
+  forever
+   #50 clock = ~clock;
+  end
+   
+ initial begin
+  // Initialize Inputs
+   
+  reset = 0;
+ 
+  // Wait 100 ns for global reset to finish
+  #100;
+      reset = 1;
+  #200;
+  reset = 0;
+  // Add stimulus here
+ 
+ end
+  
+ initial begin
+ $display("clock rnd");
+ $monitor("%b,%b", clock, rand);
+ end     
+endmodule
