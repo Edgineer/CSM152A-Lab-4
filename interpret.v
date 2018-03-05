0afe857cf046dbf13c45cbb48e7cf12d23206ccd
Edgar Lopez <Edgineer@users.noreply.github.com>
2018-03-04 21:56:41 -0800

    Add files via upload
    filename change

---
+++ interpret.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module interpretVal(
+input [7:0] data, input mode, 
+output [7:0] val, output neg
+);
+
+reg [7:0] val_reg;
+reg neg_reg;
+
+always @* begin
+	if (mode == 1) begin
+		if (data[7] == 1) begin
+			val_reg = ~data + 1;
+			neg_reg = 1;
+		end
+		else begin
+			val_reg = data;
+			neg_reg = 0;
+		end
+	end
+	else begin
+		val_reg = data;
+		neg_reg = 0;
+	end
+end
+
+assign val = val_reg;
+assign neg = neg_reg;
+
+endmodule
